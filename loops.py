2d3c29d51b498f77d89315a050fd68ad33a862b5
PruthviQABLR <Pruthviqablr@gmail.com>
2026-09-07 09:54:41 +0530

    07-09-26

--- loops.py
+++ loops.py
@@ -4,4 +4,24 @@ for i in range(0,5):
 for i in range(10,5,-1):
     print(i)
 
-    
+
+tools =["selenium","playwright","langchain","scrapy","requests","beautifulsoup"]
+
+for i in tools:
+    print(i)
+print("-----------")
+
+for i in tools:
+    if i=="requests":
+        break
+    print(i)
+print("-----------")
+
+
+area = 1
+while (area<10):
+    print(area)
+    area=area+1
+
+print("-----------")
+
